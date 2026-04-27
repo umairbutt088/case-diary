c26e54eefc58b073f1df454dc054b23b610c13ad
umaitbutt088 <muhammadumairbutt2@gmail.com>
2026-04-27 15:03:19 +0500

    bare acts added in the app to easily search the case sections

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -135,6 +135,12 @@ function RootLayoutInner() {
                       headerShown: false,
                     }}
                   />
+                  <Stack.Screen
+                    name="acts"
+                    options={{
+                      headerShown: false,
+                    }}
+                  />
                 </Stack>
               </AuthNavigator>
             </CopilotProvider>
